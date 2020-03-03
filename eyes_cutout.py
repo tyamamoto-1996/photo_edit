411cca7f3a9ec6c61b30364f6432b7f32fbdfd8f
tyamamoto-1996 <yuikeru2.takosu@gmail.com>
2020-03-03 20:51:26 +0900

    comit

---
+++ eyes_cutout.py
@@ -0,0 +1,49 @@
+import cv2
+from PIL import Image
+import os
+
+eye_cascade_path = 'C://Users//yuikeru2//Desktop//Eye_position//program//haarcascades//haarcascade_eye.xml'
+face_cascade_path = 'C://Users//yuikeru2//Desktop//Eye_position//program//haarcascades//haarcascade_frontalface_default.xml'
+
+face_cascade = cv2.CascadeClassifier(face_cascade_path)
+eye_cascade = cv2.CascadeClassifier(eye_cascade_path)
+
+photo = 'C://Users//yuikeru2//Desktop//Eye_position//program//woman.jpg'
+src = cv2.imread(photo)
+src_gray = cv2.cvtColor(src, cv2.COLOR_BGR2GRAY)
+
+eyes = eye_cascade.detectMultiScale(src_gray)
+
+
+left = 10000
+right = 0
+upper = 10000
+lower = 0
+num_of_eyes = 0
+
+for (ex, ey, ew, eh) in eyes:
+    if ew < 100 or eh < 100:    #幅100px以下、高さ100px以下のものは眼としない
+        continue
+    else:
+        cv2.rectangle(src, (ex, ey), (ex + ew, ey + eh), (0, 255, 0), 2)
+        if ex < left:
+            left = ex
+        if ey < upper:
+            upper = ey
+        if ex + ew > right:
+            right = ex + ew
+        if ey + eh > lower:
+            lower = ey + eh
+        num_of_eyes += 1
+
+if left == 10000 or upper == 10000:
+    print(os.path.basename(photo) + ' is skipped because it has 0 eyes')
+if num_of_eyes != 2:
+    print(os.path.basename(photo) + ' is skipped because it has ' + str(num_of_eyes) + ' eyes')
+    cv2.imwrite('C://Users//yuikeru2//Desktop//Eye_position//program//woman_eyes.jpg', src)
+else:
+    cv2.imwrite('C://Users//yuikeru2//Desktop//Eye_position//program//woman_eyes.jpg', src)
+
+    im = Image.open(photo)
+    im_crop = im.crop((left, upper, right, lower))
+    im_crop.save('C://Users//yuikeru2//Desktop//Eye_position//program//woman_eyes_cut.jpg', quality=95)
---
+++ rename2.py
@@ -0,0 +1,30 @@
+from PIL import Image
+import glob
+import re
+import os
+
+'''
+指定したフォルダ内のすべてのjpgファイルを0.jpg、1.jpg、…と名前を変更し
+別フォルダに保存し直すスクリプト
+'''
+#元画像フォルダ
+in_path = 'C://Users//yuikeru2//Desktop//Eye_position//program//Int_photo//*'
+#保存先フォルダ
+out_path = 'C://Users//yuikeru2//Desktop//Eye_position//program//Int_photo_rename'
+
+
+def main():
+    path = glob.glob(in_path + '//*.jpg')
+
+    k = 0
+    x = 0
+    for i in range(len(path)):
+        dir = os.path.dirname(path[x])
+        dirname = dir.lstrip('C://Users//yuikeru2//Desktop//Eye_position//program//Int_photo')
+        x += 1
+        im = Image.open(path[k])
+        im.save(out_path + '//i' + dirname[1:] + '.jpg')
+        print(os.path.basename(path[k]) + ' Done')
+        k += 1
+
+main()
